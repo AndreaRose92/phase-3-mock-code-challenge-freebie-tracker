0d33ee6b48e1bee61cb45c3ea57e0260ba8cded8
Andrea Rosentel <andrearosentel@gmail.com>
2022-08-17 11:39:57 -0700

    commit tree eba75a27164b9c301a396f150638e75ed0783521

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -3,7 +3,9 @@ class Dev < ActiveRecord::Base
     has_many :companies, through: :freebies
     
     def received_one? item_name
-        self.freebies.where(item_name: item_name).exists?
+        # self.freebies.where("item_name = ? and dev_id = ?", "#{item_name}", "#{self.id}")
+        # self.freebies.any? {|freebie| freebie.item_name == item_name}
+        
     end
 
     def give_away dev, freebie
